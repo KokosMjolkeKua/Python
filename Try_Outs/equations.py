accdf28b739df7008a63599b33d58538e60c0325
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-06 13:50:26 +0100

    Testing and was able to figure out the while loop

---
+++ Try_Outs/equations.py
@@ -0,0 +1,19 @@
+#Testing of different variables
+
+print("Trying out equations and numbers")
+
+x = 2.3
+y = 4
+z = 0.4
+
+#Checking addition to see if depending on the variable, will it give me a decimal or an INteger?
+print(x * y)
+
+#Same checking how the program responds
+print(x + y + z)
+
+#Checking %
+print((x + y) % z)
+
+#Checking minus
+print(x-y-z)
---
+++ Try_Outs/if_and_for.py
@@ -0,0 +1,24 @@
+
+#IF statements
+
+x = 5
+y = 0
+
+if x > y:
+    print(x)
+    y = y + 1 
+
+if y > 0:
+    print(y)
+
+
+#For LOOPs
+# and maybe WHile as well 
+
+t = 11
+print("Countdown:")
+while(t > 0):
+    t = t - 1
+    print(t)
+print("Launch!")
+
