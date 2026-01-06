796a6d016bc1c08d9b08226b93efe6799472f8ca
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-06 13:35:49 +0100

    Trying out some python now, first day commits

---
+++ Try_Outs/first.py
@@ -0,0 +1,20 @@
+print("Hello World.")
+print("Goodbye!")
+
+
+if 5 > 2:
+    print("5 is bigger than 2")
+
+if 5 < 2:
+    print("5 is not bigger than 2")    
+
+
+#comments are written like this
+#This is insane how easier this is than java
+#hallo??
+
+x = 5
+y = "Hello world"
+
+if x > 2:   
+    print(y)
