89d1e51f539728e5ac8f7e7de1fb25e20a09d3ba
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-06 14:19:06 +0100

    im not sure its possible, will figure it out

--- Try_Outs/print.py
+++ Try_Outs/print.py
@@ -11,3 +11,5 @@ print("Hello"); print("There"); print("You.")
 
 
 #I want to test if i can put them all on the same line or not
+print("hello"), print(" "), print("there!")
+#Ok all that did was do the same, hm so (, and ;) is the same
