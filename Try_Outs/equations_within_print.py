8d1769f1d40ad11d6c842ea7bf0af7036dbb24de
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-06 14:25:18 +0100

    trying to add text and numbers in the same print line

---
+++ Try_Outs/equations_within_print.py
@@ -0,0 +1,12 @@
+x = 9
+y = 5
+
+#Equations are not too hard, but need to do differently inside the print
+
+print(x - y), print(x * y)
+print(y + x), print(y / x)
+
+#Seperating text and numbers:
+print("Sum: ", 34, " is a number")
+
+print("Sum of ", x, " and ", y, " is : ", x + y)
