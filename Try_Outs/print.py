cad6a03a6725575be02c025f14ff14fad9245f83
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-06 14:15:20 +0100

    testing print options

---
+++ Try_Outs/print.py
@@ -0,0 +1,13 @@
+#Different ways to print
+
+print("Hello")
+print("There")
+print("You.")
+
+#adding a line to see the terminal a bit clearer
+print()
+
+print("Hello"); print("There"); print("You.")
+
+
+#I want to test if i can put them all on the same line or not
