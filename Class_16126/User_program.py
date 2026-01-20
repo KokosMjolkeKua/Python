bacad3fe433d0b70d52c97ec10006c97537006bf
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-20 13:09:41 +0100

    changed so its correct

--- Class_16126/User_program.py
+++
@@ -1,7 +0,0 @@
-name = input("Whats your name:")
-age = input("Whats your age:")
-
-year = 2026 - int(age)
-print(f"{name}, you were born the year {year} Or {year - 1}.")
-year = 2025 - int(age)
-print(f"Or the year {year}.")
--- Class_16126/first_python.py
+++
@@ -1,9 +0,0 @@
-print("Hello World!")
-
-def Calculate_average(a,b,c):
-  total = a + b + c
-  average = total / 3
-  return average
-
-print("Average is: ",Calculate_average(2,5,3)) 
-
