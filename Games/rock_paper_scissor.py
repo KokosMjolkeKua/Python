f77f09397cb8b2d5b5dbf4d00b2efd4349e1bc79
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-27 12:15:54 +0100

    update to converter

--- Games/rock_paper_scissor.py
+++ Games/rock_paper_scissor.py
@@ -1,6 +1,7 @@
 import random
-choice = str(input("Rock, Paper, Scissor?: "))
-
+win = 0
+loose = 0
+tie = 0
 
 def rock_paper_scissor(choice):
     random_int = random.randint(1,3)
@@ -8,26 +9,44 @@ def rock_paper_scissor(choice):
         # "Rock"
         if choice == "Rock":
             print("Its a Tie!")
+            tie += 1
         elif choice == "Paper":
             print("You win!")
+            win += 1
         elif choice == "Scissor":
-            print("You loose..")        
+            print("You loose..") 
+            loose += 1     
     elif random_int == 2:
         # "Paper"
         if choice == "Paper":
             print("Its a Tie!")
+            tie += 1
         elif choice == "Scissor":
             print("You win!")
+            win += 1
         elif choice == "Rock":
             print("You loose..")
+            loose += 1
         print("You guessed Correctly!")
     else:
         # "Scissor"
         if choice == "Scissor":
             print("Its a Tie!")
+            tie += 1
         elif choice == "Rock":
             print("You win!")
+            win += 1
         elif choice == "Paper":
-            print("You loose..")   
+            print("You loose..")
+            loose += 1
 
-rock_paper_scissor(choice)        
+while True:
+    choice = input("Rock, Paper, Scissor?: ")
+    rock_paper_scissor(choice)        
+    print("SCORE:")
+    print("Wins: ",win)
+    print("Losses: ",loose)
+    print("Ties: ",tie)
+    x = input("Do you wish to continue? (yes/no) : ")
+    if x == "no":
+        break
