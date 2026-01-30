06ebf7fe1744fdff48ac8203302b6daf992fab5a
KokosMjolkeKua <insmi6764@oslomet.no>
2026-01-30 08:59:57 +0100

    class

---
+++ School/30126_Class/class.py
@@ -0,0 +1,55 @@
+name="Yeshy"
+age=78
+print(name," you are ", age, " years old..S")
+
+#Docstring
+def convert_celcius_to_fahrenheit(celsius):
+    """ Converter function from Celsius to Fahrenheit
+        Returns the value. """
+    return (celsius * 9/5)+32
+
+#always use docstring to showcase your thinking 
+
+
+#Function int
+def add(a,b):
+    return a + b
+
+print(add(2,3))
+print(add(3.2,3.8))
+print(add("Hello","Yeeshy"))
+
+
+#Type Hints:
+name:str="Alice"
+age:int=30
+temp:float=37.5
+is_healthy:bool=True
+
+#Functions
+def add(a:int,b:int):
+    """Add Integers"""
+    return a + b
+
+def get_patient(id:int) -> dict:
+    """Return patient Record"""
+    return {"Id":id,"Name":"Alice"}
+
+
+
+
+#Class Example:
+name="Hello"
+print(name)
+
+name:str="HI"
+name=4 
+#it still works because python has dynamic types, 
+#but for humans we can see that something is wrong
+
+#If you go to settings, search typechecking and choose basic, the variabel will 
+#get an error, it will still run, but now you will know something is wrong
+
+print(name)
+
+
